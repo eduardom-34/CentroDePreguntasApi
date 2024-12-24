5426a0880858d4e6ee1b97e51507e1da4304b2d8
CesarLinux <eduardomelchor34@gmail.com>
2024-12-24 17:30:48 -0600

    Get Id in users

--- CentroDePreguntasApi/DTOs/UserDto.cs
+++ CentroDePreguntasApi/DTOs/UserDto.cs
@@ -4,6 +4,7 @@ namespace CentroDePreguntasApi.DTOs;
 
 public class UserDto
 {
+  public int UserId { get; set; }
   public string FirstName { get; set; }
   public string LastName { get; set; }
   public string UserName { get; set; }
--- CentroDePreguntasApi/obj/Debug/net8.0/CentroDePreguntasApi.AssemblyInfo.cs
+++ CentroDePreguntasApi/obj/Debug/net8.0/CentroDePreguntasApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CentroDePreguntasApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13c66058e9fb39e417af1f535c40d43aaaea92bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dcf235d832604433f7bf42d2f945fea1d01f58a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CentroDePreguntasApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CentroDePreguntasApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
