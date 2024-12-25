070452a5d3da0f8b471933ea2e50e10ee04b3784
CesarLinux <eduardomelchor34@gmail.com>
2024-12-25 17:03:36 -0600

    validate token

--- CentroDePreguntasApi/obj/Debug/net8.0/CentroDePreguntasApi.AssemblyInfo.cs
+++ CentroDePreguntasApi/obj/Debug/net8.0/CentroDePreguntasApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CentroDePreguntasApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61cee72710bd115edc5c16d2c6b74c96401b2ded")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f657f986bab06241e1df4c594e8c671d3db8743e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CentroDePreguntasApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CentroDePreguntasApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
